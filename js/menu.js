3ce9ce5f61664f0bc37f5306385c6b2b246ea55c
Raniel-Roque <raniel.roque0509@gmail.com>
2023-05-15 22:43:26 +0800

    test

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,11 @@
+const menuButton = document.querySelector('.navbar-toggler');
+const body = document.querySelector('body');
+
 menuButton.addEventListener('click', function() {
-    body.classList.toggle('dark');
-    if (body.classList.contains('dark')) {
-      body.classList.remove('light');
-    } else {
-      body.classList.add('light');
-    }
-  });
+  body.classList.toggle('dark');
+  if (body.classList.contains('dark')) {
+    body.classList.remove('light');
+  } else {
+    body.classList.add('light');
+  }
+});
