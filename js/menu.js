1818e3af35751bad9757f1ad06af0eae70984670
Raniel-Roque <raniel.roque0509@gmail.com>
2023-05-23 23:57:11 +0800

    Minor Fix and Animation Test

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,3 @@
-const menuButton = document.querySelector('.navbar-toggler');
-const body = document.querySelector('body');
-
-menuButton.addEventListener('click', function() {
-  body.classList.toggle('dark');
-  if (body.classList.contains('dark')) {
-    body.classList.remove('light');
-  } else {
-    body.classList.add('light');
-  }
-});
-
-$(function() {
-  $('body').scrollspy({ target: '.fade-scrollspy' });
-});
-
 window.addEventListener('scroll', revealY);
 
     function revealY() {
@@ -52,3 +36,19 @@ function revealX() {
   }
 
 }
+
+const menuButton = document.querySelector('.navbar-toggler');
+const body = document.querySelector('body');
+
+menuButton.addEventListener('click', function() {
+  body.classList.toggle('dark');
+  if (body.classList.contains('dark')) {
+    body.classList.remove('light');
+  } else {
+    body.classList.add('light');
+  }
+});
+
+$(function() {
+  $('body').scrollspy({ target: '.fade-scrollspy' });
+});
